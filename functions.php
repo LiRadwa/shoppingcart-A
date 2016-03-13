e3cd1097f80b5ab491ed96eecbe0bcdc8c604e31
FatmaArabi <arabi.1993.arabi@gmail.com>
2016-03-14 00:17:44 +0200

    function

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+// functions.php
+function theme_setup(){
+	add_theme_support('post-thumbnails');
+}
+add_action('after_setup_theme', 'theme_setup');
+
+function load_scripts_and_styles(){
+	wp_enqueue_style('style', get_stylesheet_uri());	
+}
+add_action('wp_enqueue_scripts', 'load_scripts_and_styles');
+
+
+require_once('library/index.php');
